56823e0e4cf6e2c8ed8ad88b30c75b15668a60c1
codebykaifi58 <skaif1694@gmail.com>
2025-08-29 11:23:04 +0500

    store

--- Store-app/vite.config.js
+++ Store-app/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+//👇 Replace with your GitHub repo name:
+const repoName = 'Store-app'
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`, // 👈 Important
 })
+
