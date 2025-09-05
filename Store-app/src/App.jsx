c99cd20dc07648ec544a3786667a8f353f4695b8
codebykaifi58 <skaif1694@gmail.com>
2025-09-05 10:59:57 +0500

    store

--- Store-app/src/App.jsx
+++ Store-app/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
+
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
